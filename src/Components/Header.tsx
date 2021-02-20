7df4915153775b8e97ab17ea114ba89fdd5d83a7
r19 <ahmedmhiz2010@gmail.com>
2021-02-20 23:44:43 +0300

    Building Header...

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,95 +3,178 @@ import {
   makeStyles,
   Typography,
   Theme,
-  Link as MaterialLink,
   Button,
   Menu,
   MenuItem,
+  AppBar,
+  IconButton,
+  Toolbar,
+  FormGroup,
+  FormControlLabel,
+  Switch,
 } from "@material-ui/core";
+import { AccountCircle } from "@material-ui/icons";
+import MenuIcon from "@material-ui/icons/Menu";
 
 import React from "react";
 import { Link } from "react-router-dom";
-import { links } from "../Routes";
 import "./Header.scss";
 
-const useStyles = makeStyles((th: Theme) => {
-  return createStyles({
-    root: {
-      background: "linear-gradient(90deg, #023DC3, #00C8E0)",
-      color: "#FFF",
-      borderBottomLeftRadius: "40px",
-      borderBottomRightRadius: "40px",
-      overflow: "hidden",
-      padding: "20px",
-      display: "flex",
-      justifyContent: "space-evenly",
-    },
-    link: {
-      color: "#FFF",
-      textDecoration: "none",
-      fontSize: "1.3rem",
-    },
-  });
-});
-
-function SimpleMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const styles = useStyles();
-  return (
-    <div>
-      <Button
-        className={styles.link}
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Open Menu
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-    </div>
-  );
+interface HeaderLink {
+  url: string;
+  title?: string;
+  subLinks?: HeaderLink[];
 }
 
+const links: HeaderLink[] = [
+  { url: "/services", title: "خدمات" },
+  { url: "/employees", title: "موظفين" },
+  { url: "/faults", title: "أعطال" },
+  { url: "/statistics", title: "احصائيات" },
+  { url: "/other", title: "أخرى" },
+  {
+    url: "/watches",
+    title: "مراقبات",
+    subLinks: [
+      { url: "/watches/a/", title: "Ahmadi" },
+      { url: "/watches/b/", title: "Farwaniya" },
+      { url: "/watches/c/", title: "Hawalli" },
+    ],
+  },
+];
+
 const Header: React.FC = (props) => {
-  const styles = useStyles();
-  const Extra = () => {
+  // const styles = useStyles();
+
+  const HeaderSubElement: React.FC = (props) => {
     return (
-      <React.Fragment>
+      <div className="sub-header">
+        <a href="#">Go There</a>
+        <a href="#">Go There 1</a>
+        <a href="#">Go There 2</a>
+      </div>
+    );
+  };
+  const HeaderElements: React.FC = () => {
+    return (
+      <>
         {links.map((link, index) => {
           return (
-            <Link to={link.url} className={styles.link} key={index}>
-              {link.title ?? "Empty"}
-            </Link>
+            <div className="item" key={index}>
+              <Link to={link.url}>{link.title ?? "Empty"}</Link>
+              <div className="sub">
+                <HeaderSubElement />
+              </div>
+            </div>
           );
         })}
-      </React.Fragment>
+      </>
     );
   };
   return (
-    <div className={styles.root}>
-      {/* <Typography variant="h2">Header Element</Typography> */}
-      <Extra />
-      <SimpleMenu />
+    <div className="Header">
+      <div className="main">
+        {/* <Typography variant="h2">Header Element</Typography> */}
+        <HeaderElements />
+      </div>
     </div>
   );
 };
 
-export default Header;
+const Header2: React.FC = () => {
+  const Logo = () => {
+    return (
+      <div className="navbar-brand">
+        <a className="navbar-item" href="#">
+          <img
+            src="https://www.mew.gov.kw/images/logo3.png"
+            // width="112"
+            height="28"
+          ></img>
+        </a>
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    );
+  };
+  const Elements = () => {
+    const Basic: React.FC = (props) => {
+      return (
+        <a className="navbar-item" href="#">
+          {props.children}
+        </a>
+      );
+    };
+    const Composed: React.FC<{ title: string; links: HeaderLink }> = (
+      props
+    ) => {
+      return (
+        <div className="navbar-item has-dropdown is-hoverable">
+          <a className="navbar-link">{props.title}</a>
+          <div className="navbar-dropdown">
+            {links.map((link, index) => {
+              return (
+                <a className="navbar-item" href={link.url} key={index}>
+                  {link.title}
+                </a>
+              );
+            })}
+            <hr className="navbar-divider"></hr>
+          </div>
+        </div>
+      );
+    };
+    const NavStart = () => {
+      return (
+        <div className="navbar-start">
+          <Basic>Home</Basic>
+          <Basic>Documentaion</Basic>
+          <Composed title="asd" links={links[links.length-1]} />
+          <Composed title="asd" links={links[links.length-1]} />
+        </div>
+      );
+    };
+    const NavEnd = () => {
+      return (
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
+          </div>
+        </div>
+      );
+    };
+    return (
+      <div id="navbarBasicExample" className="navbar-menu">
+        <NavStart />
+        <NavEnd />
+      </div>
+    );
+  };
+  return (
+    <nav
+      className="navbar is-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <Logo />
+      <Elements />
+    </nav>
+  );
+};
+
+export default Header2;
