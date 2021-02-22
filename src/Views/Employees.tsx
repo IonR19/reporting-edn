c57dc9c1bfa6f15d7a1b00bf2f841b87cec1b504
r19 <ahmedmhiz2010@gmail.com>
2021-02-23 00:39:04 +0300

    Added Form on Employees Page

--- src/Views/Employees.tsx
+++ src/Views/Employees.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { WorkPlaces } from "../Models/WorkPlaces";
-import { DataGrid } from "@material-ui/data-grid";
-import { useDemoData } from "@material-ui/x-grid-data-generator";
-import "regenerator-runtime";
+import { ColDef, DataGrid, RowsProp } from "@material-ui/data-grid";
+import axios from "axios";
+import { Formik } from "formik";
+import { API } from "../config";
 
 const Table = () => {
   const TableHead = () => {
@@ -49,54 +50,126 @@ const Table = () => {
     </table>
   );
 };
-
 const EmployeeDataGrid = () => {
-  const { data } = useDemoData({
-    dataSet: "Commodity",
-    rowLength: 100,
-    maxColumns: 6,
-  });
+  let defaultRows: RowsProp = [
+    { id: 91, civilId: "Hello", name: "World" },
+    { id: 92, civilId: "XGrid", name: "is Awesome" },
+    { id: 93, civilId: "Material-UI", name: "is Amazing" },
+  ];
+
+  let [rows, setRows] = useState(defaultRows);
+  const [apiCalled, setApiCalled] = useState(false);
+  if (!apiCalled) {
+    axios.get(`${API}/employees`).then(async (res) => {
+      await setApiCalled(true);
+      rows = [...rows, ...res.data];
+      setRows(rows);
+    });
+  }
+
+  const columns: ColDef[] = [
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "civilId", headerName: "Civil ID", width: 150, sortable: false },
+    { field: "name", headerName: "Name", width: 220 },
+  ];
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        {...data}
-        filterModel={{
-          items: [{ columnField: "commodity", operatorValue: "contains", value: "rice" }],
+    <div style={{ height: "70vh", width: "90%", margin: "auto" }}>
+      <DataGrid columns={columns} rows={rows} />
+    </div>
+  );
+};
+
+const FilterComponent: React.FC = () => {
+  let watches = Object.keys(WorkPlaces.watch);
+  let section = Object.keys(WorkPlaces.section);
+  const [selection, setSelection] = useState("select");
+  const [innerSelection, setInnerSelection] = useState("");
+
+  return (
+    <div className="select">
+      <select
+        value={selection}
+        onChange={(e) => {
+          setSelection(e.target.value);
+          setInnerSelection(e.target.selectedOptions[0].dataset.link!);
         }}
-      />
+      >
+        {watches.map((watchCode) => (
+          <option data-link={watchCode} key={watchCode}>
+            {WorkPlaces.watch[watchCode].title}
+          </option>
+        ))}
+        {section.map((sectionCode) => (
+          <option data-link={sectionCode} key={sectionCode}>
+            {WorkPlaces.section[sectionCode].title}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+const AddEmployeeForm = () => {
+  const [values, setValues] = useState({
+    name: "",
+    civilId: "",
+  });
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios.post(`${API}/employees`, values).then(async (res) => {
+      console.log(res.data);
+    });
+  };
+  return (
+    <div style={{ margin: "auto" }}>
+      <form onSubmit={onSubmit}>
+        <div className="field">
+          <label className="label" htmlFor="name">
+            Full Name
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              name="name"
+              id="name"
+              value={values.name}
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label" htmlFor="civilId">
+            Civil ID
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              name="civilId"
+              id="civilId"
+              value={values.civilId}
+              onChange={(e) =>
+                setValues({ ...values, civilId: e.target.value })
+              }
+            />
+          </div>
+        </div>
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
 
 function Employees() {
-  let watches = Object.keys(WorkPlaces.watch);
-  let section = Object.keys(WorkPlaces.section);
-
-  const [selection, setSelection] = useState("select");
   return (
     <div>
-      <div className="select">
-        <select
-          value={selection}
-          onChange={(e) => {
-            setSelection(e.target.selectedOptions[0].dataset.link!);
-          }}
-        >
-          {watches.map((watchCode) => (
-            <option data-link={watchCode} key={watchCode}>
-              {WorkPlaces.watch[watchCode].title}
-            </option>
-          ))}
-          {section.map((sectionCode) => (
-            <option data-link={sectionCode} key={sectionCode}>
-              {WorkPlaces.section[sectionCode].title}
-            </option>
-          ))}
-        </select>
-      </div>
-      <Table />
+      <FilterComponent />
+      {/* <Table /> */}
       <EmployeeDataGrid />
+      <AddEmployeeForm />
     </div>
   );
 }
