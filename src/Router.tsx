658e7a2b9a2d1ec43d54c2aee9157fb1fbb2fb0b
r19 <ahmedmhiz2010@gmail.com>
2021-03-01 07:32:20 +0300

    added watch route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import Faults from "./Views/Faults";
 import Employees from "./Views/Employees";
+import Watches from "./Views/Watches";
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
         <Route path="/" exact component={() => <h1>Main Page</h1>} />
         <Route path="/employees" component={Employees} />
         <Route path="/faults" component={Faults} />
+        <Route path="/watches" component={Watches} />
       </Route>
     </Switch>
   );
